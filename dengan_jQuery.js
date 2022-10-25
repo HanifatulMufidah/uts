3fe76119ac91486ec04f46f5fdee259111f64ea9
nasehqusyairy <nasyikhqusyairy@gmail.com>
2022-10-25 13:51:16 +0700

    JS dan JQuery

---
+++ dengan_jQuery.js
@@ -0,0 +1,28 @@
+$('#submit').click(() => {
+  $('#result').html($('textarea').val())
+})
+
+$('#size').change((e) => {
+  $('textarea').css('font-size', e.target.value)
+  $('#result').css('font-size', e.target.value)
+})
+
+$('#color').change((e) => {
+  $('textarea').css('color', e.target.value)
+  $('#result').css('color', e.target.value)
+})
+
+$('#bold').click(() => {
+  $('textarea').toggleClass('bold')
+  $('#result').toggleClass('bold')
+})
+
+$('#italic').click(() => {
+  $('textarea').toggleClass('italic')
+  $('#result').toggleClass('italic')
+})
+
+$('#under').click(() => {
+  $('textarea').toggleClass('under')
+  $('#result').toggleClass('under')
+})
