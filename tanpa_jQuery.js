18a3b14154d434ee8b57ce9c77887304123e6e98
nasehqusyairy <nasyikhqusyairy@gmail.com>
2022-10-25 14:26:39 +0700

    DOMContentLoaded

--- tanpa_jQuery.js
+++ tanpa_jQuery.js
@@ -1,38 +1,41 @@
-const tombol_submit = document.getElementById('submit')
-const div_result = document.getElementById('result')
-const textarea = document.querySelector('textarea')
-const select_font_size = document.getElementById('size')
-const select_color = document.getElementById('color')
+document.addEventListener('DOMContentLoaded', () => {
+  const tombol_submit = document.getElementById('submit')
+  const div_result = document.getElementById('result')
+  const textarea = document.querySelector('textarea')
+  const select_font_size = document.getElementById('size')
+  const select_color = document.getElementById('color')
 
-const bold = document.getElementById('bold')
-const italic = document.getElementById('italic')
-const under = document.getElementById('under')
+  const bold = document.getElementById('bold')
+  const italic = document.getElementById('italic')
+  const under = document.getElementById('under')
 
-tombol_submit.addEventListener('click', () => {
-  div_result.innerHTML = textarea.value
-})
+  tombol_submit.addEventListener('click', () => {
+    div_result.innerHTML = textarea.value
+  })
 
-select_font_size.addEventListener('change', (e) => {
-  textarea.style.fontSize = e.target.value
-  div_result.style.fontSize = e.target.value
-})
+  select_font_size.addEventListener('change', (e) => {
+    textarea.style.fontSize = e.target.value
+    div_result.style.fontSize = e.target.value
+  })
 
-select_color.addEventListener('change', (e) => {
-  textarea.style.color = e.target.value
-  div_result.style.color = e.target.value
-})
+  select_color.addEventListener('change', (e) => {
+    textarea.style.color = e.target.value
+    div_result.style.color = e.target.value
+  })
 
-bold.addEventListener('click', () => {
-  textarea.classList.toggle('bold')
-  div_result.classList.toggle('bold')
-})
+  bold.addEventListener('click', () => {
+    textarea.classList.toggle('bold')
+    div_result.classList.toggle('bold')
+  })
 
-italic.addEventListener('click', () => {
-  textarea.classList.toggle('italic')
-  div_result.classList.toggle('italic')
-})
+  italic.addEventListener('click', () => {
+    textarea.classList.toggle('italic')
+    div_result.classList.toggle('italic')
+  })
 
-under.addEventListener('click', () => {
-  textarea.classList.toggle('under')
-  div_result.classList.toggle('under')
+  under.addEventListener('click', () => {
+    textarea.classList.toggle('under')
+    div_result.classList.toggle('under')
+  })
+
 })
