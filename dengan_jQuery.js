9fcc69a4fc53ed0b50b3598f50d715efb9601cbc
nasehqusyairy <nasyikhqusyairy@gmail.com>
2022-10-25 13:53:48 +0700

    modified:   dengan_jQuery.js

--- dengan_jQuery.js
+++ dengan_jQuery.js
@@ -1,28 +1,31 @@
+const textarea = $('textarea')
+const result = $('#result')
+
 $('#submit').click(() => {
-  $('#result').html($('textarea').val())
+  result.html(textarea.val())
 })
 
 $('#size').change((e) => {
-  $('textarea').css('font-size', e.target.value)
-  $('#result').css('font-size', e.target.value)
+  textarea.css('font-size', e.target.value)
+  result.css('font-size', e.target.value)
 })
 
 $('#color').change((e) => {
-  $('textarea').css('color', e.target.value)
-  $('#result').css('color', e.target.value)
+  textarea.css('color', e.target.value)
+  result.css('color', e.target.value)
 })
 
 $('#bold').click(() => {
-  $('textarea').toggleClass('bold')
-  $('#result').toggleClass('bold')
+  textarea.toggleClass('bold')
+  result.toggleClass('bold')
 })
 
 $('#italic').click(() => {
-  $('textarea').toggleClass('italic')
-  $('#result').toggleClass('italic')
+  textarea.toggleClass('italic')
+  result.toggleClass('italic')
 })
 
 $('#under').click(() => {
-  $('textarea').toggleClass('under')
-  $('#result').toggleClass('under')
+  textarea.toggleClass('under')
+  result.toggleClass('under')
 })
